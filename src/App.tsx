c747218ea336687450df063d04402e606c223ca3
Ethan Standel <ethan@standel.io>
2022-06-16 01:26:16 -0400

    fix: non-1:1 joystick

--- src/App.tsx
+++ src/App.tsx
@@ -76,6 +76,7 @@ const styles: Record<string, JSX.CSSProperties> = {
     "border-radius": "50%"
   },
   joystickHandle: {
+    padding: "0",
     width: "100%",
     height: "100%",
     display: "flex",
